ea30442e94e0e5ccf24efd3b7fa79d2e3339658c
WLQ <836200779@qq.com>
2022-09-14 17:19:27 +0800

    Test maven-cache and seperated deploy in CI/CD

--- backend/sustech-regency-main/src/main/java/com/sustech/regency/controller/MerchantController.java
+++ backend/sustech-regency-main/src/main/java/com/sustech/regency/controller/MerchantController.java
@@ -2,24 +2,19 @@ package com.sustech.regency.controller;
 
 import com.sustech.regency.db.po.Hotel;
 import com.sustech.regency.service.MerchantService;
-import com.sustech.regency.service.UserService;
 import com.sustech.regency.web.vo.ApiResponse;
 import io.swagger.annotations.ApiOperation;
-import io.swagger.annotations.ApiParam;
-import org.hibernate.validator.constraints.Range;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.Map;
 
+//Todo: API安全性校验是否是商家
 @Validated //单参数校验时必须加上该注解才会生效:https://developer.aliyun.com/article/786719
 @RestController
-@RequestMapping("/merchant") //调用时会使用这个名字做前缀
-
+@RequestMapping("/merchant")
 public class MerchantController {
     @Autowired
     private MerchantService merchantService;
