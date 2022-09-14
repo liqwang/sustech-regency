07d5716cdd154c0895e8a212703f02d2a85da11b
WLQ <836200779@qq.com>
2022-09-14 16:37:37 +0800

    Add dependency `spring-boot-starter-mail`

---
+++ backend/sustech-regency-main/src/test/java/com/sustech/regency/MailTests.java
@@ -0,0 +1,23 @@
+package com.sustech.regency;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+
+@SpringBootTest
+public class MailTests {
+	@Autowired
+	private JavaMailSender javaMailSender;
+
+	@Test
+	void test(){
+		SimpleMailMessage message = new SimpleMailMessage();
+		message.setFrom("836200779@qq.com");
+		message.setTo("12011619@mail.sustech.edu.cn");
+		message.setSubject("主题:yfsb");
+		message.setText("yfsb");
+		javaMailSender.send(message);
+	}
+}
