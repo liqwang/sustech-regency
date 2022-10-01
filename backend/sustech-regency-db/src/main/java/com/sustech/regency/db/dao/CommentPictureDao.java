fab7a7f2a7a360e04e797d0cab8b28289451ec00
WLQ <836200779@qq.com>
2022-10-01 21:41:55 +0800

    Backend: Change PO and DAO of `CommentAttachment`

--- backend/sustech-regency-db/src/main/java/com/sustech/regency/db/dao/CommentPictureDao.java
+++
@@ -1,7 +0,0 @@
-package com.sustech.regency.db.dao;
-
-import com.github.yulichang.base.MPJBaseMapper;
-import com.sustech.regency.db.po.CommentPicture;
-
-public interface CommentPictureDao extends MPJBaseMapper<CommentPicture> {
-}
--- backend/sustech-regency-db/src/main/java/com/sustech/regency/db/dao/CommentVideoDao.java
+++
@@ -1,7 +0,0 @@
-package com.sustech.regency.db.dao;
-
-import com.github.yulichang.base.MPJBaseMapper;
-import com.sustech.regency.db.po.CommentVideo;
-
-public interface CommentVideoDao extends MPJBaseMapper<CommentVideo> {
-}
