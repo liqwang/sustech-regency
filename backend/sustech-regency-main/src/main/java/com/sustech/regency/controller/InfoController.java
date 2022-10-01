23d4cd8026c5323337cdf3f87aa2c91eefafebf2
WLQ <836200779@qq.com>
2022-10-01 21:47:33 +0800

    Backend: Complete API `/info/file` to get file from URL

--- backend/sustech-regency-main/src/main/java/com/sustech/regency/controller/InfoController.java
+++ backend/sustech-regency-main/src/main/java/com/sustech/regency/controller/InfoController.java
@@ -1,18 +1,25 @@
 package com.sustech.regency.controller;
 
+import cn.hutool.core.io.FileUtil;
 import com.sustech.regency.db.dao.CityDao;
 import com.sustech.regency.db.dao.ProvinceDao;
 import com.sustech.regency.db.dao.RegionDao;
 import com.sustech.regency.db.po.City;
 import com.sustech.regency.db.po.Province;
 import com.sustech.regency.db.po.Region;
+import com.sustech.regency.web.handler.ApiException;
 import com.sustech.regency.web.vo.ApiResponse;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.File;
+import java.io.IOException;
 import java.util.List;
 
 @RestController
@@ -44,5 +51,33 @@ public class InfoController {
 	public ApiResponse<List<Region>> getAllRegions(){
 		List<Region> regions = regionDao.selectList(null);
 		return ApiResponse.success(regions);
+	}
+
+	@Value("${file-root-path}")
+	private String fileRootPath; //保存文件的根路径
+	@SuppressWarnings("CommentedOutCode")
+	@ApiOperation(value = "获取文件",hidden = true)
+	@GetMapping("/file/**")
+	public ApiResponse getFile(HttpServletResponse response, HttpServletRequest request){
+		String path = request.getRequestURI().replace("/info/file", "");
+		File file = new File(fileRootPath+path);
+		if(!file.exists()){
+			throw ApiException.badRequest("文件不存在");
+		}
+		response.reset();
+		response.setContentLength((int)file.length());
+		response.setContentType("application/octet-stream"); //MIME类型，这里表示除文本文件外的默认值
+//		response.setCharacterEncoding("utf-8");
+//		response.setHeader("Content-Disposition","inline"); //在浏览器中直接打开而不下载，但是对application/octet-stream的MIME类型无效
+//		response.setHeader("Content-Disposition","attachment; filename="+newName); //作为附件下载，并重新命名为newName
+
+		byte[] bytes = FileUtil.readBytes(file);
+		try {
+			response.getOutputStream().write(bytes);
+		} catch (IOException e) {
+			e.printStackTrace();
+			throw ApiException.INTERNAL_SEVER_ERROR;
+		}
+		return ApiResponse.success();
 	}
 }
