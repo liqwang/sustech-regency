0ae8ce4b153061c21dd0403c25bce41132af7268
RockyCYG <12011625@mail.sustech.edu.cn>
2022-10-08 13:33:19 +0800

    compile InfoImporterApplication.java

--- backend/sustech-regency-info-importer/src/main/java/com/sustech/regency/InfoImporterApplication.java
+++ backend/sustech-regency-info-importer/src/main/java/com/sustech/regency/InfoImporterApplication.java
@@ -11,12 +11,12 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 import org.mybatis.spring.annotation.MapperScan;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.annotation.Resource;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -26,54 +26,56 @@ import java.util.stream.Collectors;
 @MapperScan("com.sustech.regency.db.dao")
 @SpringBootApplication
 public class InfoImporterApplication implements ApplicationRunner {
-	@Resource
-	private ProvinceDao provinceDao;
-	@Resource
-	private CityDao cityDao;
-	@Resource
-	private RegionDao regionDao;
+    @Resource
+    private ProvinceDao provinceDao;
+    @Resource
+    private CityDao cityDao;
+    @Resource
+    private RegionDao regionDao;
 
-	public static void main(String[] args) {
-		SpringApplication.run(InfoImporterApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(InfoImporterApplication.class, args);
+    }
 
-	@Override
-	@SuppressWarnings("SpellCheckingInspection")
-	public void run(ApplicationArguments args) throws IOException {
-		Document document = Jsoup.parse(Files.readString(Path.of("backend/sustech-regency-info-importer/src/main/resources/info.html")));
-		Elements proviceDoms = document.select("#area > div.onebox > div.container");
-		proviceDoms.remove(proviceDoms.size()-1); //去除最后的"海外"，剩余34个省
-		for (Element proviceDom : proviceDoms) {
-			Province province = new Province();
-			province.setName(proviceDom.select("em").get(0).ownText());
-			System.out.println("------------------"+province.getName()+" 开始------------------");
-			provinceDao.insert(province);
+    @Override
+    @SuppressWarnings("SpellCheckingInspection")
+    public void run(ApplicationArguments args) throws IOException {
+        Document document = Jsoup.parse(Files.readString(Path.of("backend/sustech-regency-info-importer/src/main/resources/info.html")));
+        Elements proviceDoms = document.select("#area > div.onebox > div.container");
+        proviceDoms.remove(proviceDoms.size() - 1); //去除最后的"海外"，剩余34个省
+        for (Element proviceDom : proviceDoms) {
+            Province province = new Province();
+            province.setName(proviceDom.select("em").get(0).ownText());
+            System.out.println("------------------" + province.getName() + " 开始------------------");
+            provinceDao.insert(province);
 
-			List<String> cityNames = proviceDom.select("div.twoBox > em.two").stream()
-									.map(Element::ownText)
-									.collect(Collectors.toList());
-			Elements cityDoms = proviceDom.select("div.twoBox > div.thrBox");
-			for(int i=0;i<cityNames.size();i++){
-				City city = new City();
-				city.setProvinceId(province.getId());
-				city.setName(cityNames.get(i));
-				if(i==0){ //第一个DOM为省会
-					city.setIsProvincialCapital(true);
-				}
-				cityDao.insert(city);
-				Element cityDom = cityDoms.get(i);
+            List<String> cityNames = proviceDom.select("div.twoBox > em.two").stream()
+                    .map(Element::ownText)
+                    .collect(Collectors.toList());
+            Elements cityDoms = proviceDom.select("div.twoBox > div.thrBox");
+            for (int i = 0; i < cityNames.size(); i++) {
+                City city = new City();
+                city.setProvinceId(province.getId());
+                city.setName(cityNames.get(i));
+                if (i == 0) { //第一个DOM为省会
+                    city.setIsProvincialCapital(true);
+                }
+                cityDao.insert(city);
+                Element cityDom = cityDoms.get(i);
 
-				Elements regionDoms = cityDom.select("em.thr");
-				for (Element regionDom : regionDoms) {
-					String resionName = regionDom.ownText();
-					if(resionName.equals("其他区")||resionName.equals("其它区")){continue;}
-					Region region = new Region();
-					region.setCityId(city.getId());
-					region.setName(resionName);
-					regionDao.insert(region);
-				}
-				System.out.println(city.getName()+" 完成");
-			}
-		}
-	}
+                Elements regionDoms = cityDom.select("em.thr");
+                for (Element regionDom : regionDoms) {
+                    String resionName = regionDom.ownText();
+                    if (resionName.equals("其他区") || resionName.equals("其它区")) {
+                        continue;
+                    }
+                    Region region = new Region();
+                    region.setCityId(city.getId());
+                    region.setName(resionName);
+                    regionDao.insert(region);
+                }
+                System.out.println(city.getName() + " 完成");
+            }
+        }
+    }
 }
