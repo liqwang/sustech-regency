3be24a455e2a35a88fba13710245a13d4d8706ce
WLQ <836200779@qq.com>
2022-10-16 19:13:14 +0800

    Backend: Use `FileUtil` to refactor API `/consumer/upload`

--- backend/sustech-regency-main/src/main/java/com/sustech/regency/service/impl/ConsumerServiceImpl.java
+++ backend/sustech-regency-main/src/main/java/com/sustech/regency/service/impl/ConsumerServiceImpl.java
@@ -1,70 +1,37 @@
 package com.sustech.regency.service.impl;
 
-import cn.hutool.core.io.FileUtil;
 import com.sustech.regency.db.dao.CommentAttachmentDao;
 import com.sustech.regency.db.po.CommentAttachment;
 import com.sustech.regency.service.ConsumerService;
-import com.sustech.regency.web.handler.ApiException;
-import org.springframework.beans.factory.annotation.Value;
+import com.sustech.regency.util.FileUtil;
 import org.springframework.stereotype.Service;
-import org.springframework.web.context.request.RequestContextHolder;
-import org.springframework.web.context.request.ServletRequestAttributes;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.annotation.Resource;
-import javax.servlet.http.HttpServletRequest;
-import java.io.File;
-import java.io.IOException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
 import java.util.Set;
-import java.util.UUID;
+
+import static com.sustech.regency.util.FileUtil.getUUID;
+import static cn.hutool.core.io.FileUtil.getSuffix;
+import static com.sustech.regency.web.util.AssertUtil.asserts;
 
 @Service
 public class ConsumerServiceImpl implements ConsumerService {
 
 	private static final Set<String> VALID_PICTURE_SUFFIXES=Set.of("jpg","jpeg","png");
-	private static final Set<String> VALID_VIDEO_SUFFIXES=Set.of("mkv","avi","mp4"); //Todo:暂定
-	private static final SimpleDateFormat DATE_FORMAT=new SimpleDateFormat("/yyyy/MM/dd/");
-	@Value("${file-root-path}")
-	private String fileRootPath; //保存文件的根路径
+	private static final Set<String> VALID_VIDEO_SUFFIXES=Set.of("mkv","avi","mp4","mov","wmv");
 	@Resource
 	private CommentAttachmentDao commentAttachmentDao;
+	@Resource
+	private FileUtil fileUtil;
 	@Override
-	@SuppressWarnings("ResultOfMethodCallIgnored")
 	public String uploadFile(MultipartFile file,Integer orderId){
 		String originalFilename = file.getOriginalFilename();
-		String suffix = FileUtil.getSuffix(originalFilename);
-		CommentAttachment commentAttachment = new CommentAttachment();
-		if(VALID_PICTURE_SUFFIXES.contains(suffix)){
-			commentAttachment.setIsPicture(true);
-		}else if(VALID_VIDEO_SUFFIXES.contains(suffix)){
-			commentAttachment.setIsPicture(false);
-		}else{throw ApiException.badRequest("文件格式不支持");}
-		//为防止文件重名，使用UUID重命名
-		String uuid = UUID.randomUUID().toString().replace("-", "");
-		String newFileName = uuid + "." + suffix;
-
-		String dateDir = DATE_FORMAT.format(new Date());
-		@SuppressWarnings("ConstantConditions")
-		HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getRequest();
-		String baseDir = fileRootPath + dateDir;
-		File folder = new File(fileRootPath + dateDir);
-		if(!folder.exists()){
-			folder.mkdirs();
-		}
-		try {
-			File dist = new File(baseDir + newFileName);
-			FileUtil.writeBytes(file.getBytes(),dist);
-		} catch (IOException e) {
-			e.printStackTrace();
-			throw ApiException.INTERNAL_SEVER_ERROR;
-		}
-		commentAttachment.setId(uuid);
-		commentAttachment.setOrderId(orderId);
-		commentAttachment.setUploadTime(new Date());
-		commentAttachment.setSuffix(suffix);
-		commentAttachmentDao.insert(commentAttachment);
-		return request.getScheme() + "://" + request.getServerName() + ":" + request.getServerPort()+ "/info/file" + dateDir + newFileName;
+		String suffix = getSuffix(originalFilename);
+		asserts(VALID_PICTURE_SUFFIXES.contains(suffix) || VALID_VIDEO_SUFFIXES.contains(suffix),
+				"文件格式不支持");
+		String uuid = getUUID();
+		String url = fileUtil.uploadFile(file,uuid);
+		commentAttachmentDao.insert(new CommentAttachment(uuid,orderId));
+		return url;
 	}
 }
