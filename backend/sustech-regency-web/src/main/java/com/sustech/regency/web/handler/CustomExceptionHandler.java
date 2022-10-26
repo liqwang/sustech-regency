4b4a31fd7d6b3a9c5aac183e6e6bcc3d5d806d6b
WLQ <836200779@qq.com>
2022-10-26 23:25:55 +0800

    Backend: Handle `MissingServletRequestParameterException`

--- backend/sustech-regency-web/src/main/java/com/sustech/regency/web/handler/CustomExceptionHandler.java
+++ backend/sustech-regency-web/src/main/java/com/sustech/regency/web/handler/CustomExceptionHandler.java
@@ -3,21 +3,30 @@ package com.sustech.regency.web.handler;
 import com.sustech.regency.web.vo.ApiResponse;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import javax.validation.ConstraintViolationException;
 
-//只能捕获到API阶段的Exception, 无法捕获filter链中的Exception
+/**
+ * 只能捕获到API阶段的Exception, 无法捕获filter链中的Exception<p>
+ * <a href="https://www.nicechiblog.com/article/14/article_1598135490604.html">Spring中的ExceptionHandler</a>
+ */
 @RestControllerAdvice
-public class MyExceptionHandler {
+public class CustomExceptionHandler {
+
+    @ExceptionHandler(MissingServletRequestParameterException.class)
+    public ApiResponse handleMissingServletRequestParameterException(MissingServletRequestParameterException e){
+        return new ApiResponse(400,"Missing param ["+e.getParameterName()+"]");
+    }
 
     @SuppressWarnings("ConstantConditions")
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ApiResponse handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
         FieldError fieldError = e.getBindingResult().getFieldError();
         String field = fieldError.getField();
-        return new ApiResponse(400, "["+field+"] "+ fieldError.getDefaultMessage());
+        return new ApiResponse(400, "["+field+"]"+ fieldError.getDefaultMessage());
     }
 
     @ExceptionHandler(ConstraintViolationException.class)
