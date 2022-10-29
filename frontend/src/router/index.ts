b752cd637a65fcb0835349d564a45a5670cafa94
fan-git-push <yangfancool@outlook.com>
2022-10-29 21:25:30 +0800

    add chat page

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,53 +1,56 @@
-import {
-  createRouter, createWebHashHistory,
-  RouteRecordRaw,
-} from 'vue-router';
-import LoginPage from '../pages/LoginPage.vue';
-import ChangePassword from '../pages/ChangePassword.vue';
-import register from '../pages/register3.vue';
+import { createRouter, createWebHashHistory, RouteRecordRaw } from 'vue-router'
+import LoginPage from '../pages/LoginPage.vue'
+import ChangePassword from '../pages/ChangePassword.vue'
+import register from '../pages/register3.vue'
 import merchant from '../pages/merchant.vue'
 import Front from '../pages/front/Front.vue'
+import chat from '../pages/chat.vue'
 
 const routes: RouteRecordRaw[] = [
   {
     path: '/login',
     name: 'login',
-    component: LoginPage,
+    component: LoginPage
   },
   {
     path: '/merchant',
     name: 'merchant',
-    component: merchant,
+    component: merchant
   },
   {
     path: '/signup',
     name: 'signup',
-    component: register,
+    component: register
   },
   {
     path: '/changepassword',
     name: 'changepassword',
-    component: ChangePassword,
+    component: ChangePassword
+  },
+  {
+    path: '/chat',
+    name: 'chat',
+    component: chat
   },
   {
     path: '/',
     name: 'front',
     component: Front
   }
-];
+]
 
 const router = createRouter({
   history: createWebHashHistory(),
-  routes: routes,
-});
+  routes: routes
+})
 
 router.beforeEach((to, from, next) => {
-  const token = localStorage.getItem('token');
-  if (token || to.path === '/' || to.path === '/login' || to.path === '/signup' || to.path === '/changepassword') {
-    next();
+  const token = localStorage.getItem('token')
+  if (token || to.path === '/' || to.path === '/login' || to.path === '/signup' || to.path === '/changepassword' || to.path === '/chat') {
+    next()
   } else {
-    next('/login');
+    next('/login')
   }
-});
+})
 
-export default router;
+export default router
