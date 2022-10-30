c4da365c8ad293adf6d8e1ad8848f5a3b6f37ebc
RockyCYG <12011625@mail.sustech.edu.cn>
2022-10-30 17:21:55 +0800

    add user page

---
+++ frontend/src/pages/front/user/UserPage.vue
@@ -0,0 +1,55 @@
+<template>
+  <el-container class="layout-container-demo" style="height: 100%">
+    <el-aside width="200px">
+      <UserAside />
+    </el-aside>
+
+    <el-container>
+      <el-header style="text-align: right; font-size: 12px">
+        <div class="toolbar">
+          <UserIcon />
+        </div>
+      </el-header>
+
+      <el-main>
+        <router-view></router-view>
+      </el-main>
+    </el-container>
+  </el-container>
+</template>
+
+<script lang="ts" setup>
+import { ref } from 'vue'
+import { Setting } from '@element-plus/icons-vue'
+import UserAside from './UserAside.vue'
+import UserIcon from '../../../components/UserIcon.vue';
+</script>
+
+<style scoped>
+.layout-container-demo .el-header {
+  position: relative;
+  background-color: var(--el-color-primary-light-7);
+  color: var(--el-text-color-primary);
+}
+
+.layout-container-demo .el-aside {
+  color: var(--el-text-color-primary);
+  background: var(--el-color-primary-light-8);
+}
+
+.layout-container-demo .el-menu {
+  border-right: none;
+}
+
+.layout-container-demo .el-main {
+  padding: 0;
+}
+
+.layout-container-demo .toolbar {
+  display: inline-flex;
+  align-items: center;
+  justify-content: center;
+  height: 100%;
+  right: 20px;
+}
+</style>
