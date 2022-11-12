19bec9d2e10b63976db7dc1034a6ad7f6163ce1c
WLQ <836200779@qq.com>
2022-11-12 20:55:42 +0800

    Backend: Add examples of ReserveParam

--- backend/sustech-regency-main/src/main/java/com/sustech/regency/model/param/Cohabitant.java
+++ backend/sustech-regency-main/src/main/java/com/sustech/regency/model/param/Cohabitant.java
@@ -10,11 +10,11 @@ import javax.validation.constraints.Size;
 public class Cohabitant {
 
 	//Todo:其实这里应该使用严谨的身份证号校验@IdNumber
-	@ApiModelProperty(value = "同住人身份证号", required = true)
+	@ApiModelProperty(value = "同住人身份证号", required = true, example = "114514200204181234")
 	@NotEmpty @Size(min = 18, max = 18, message = "必须为18位") //Todo:封装固定长度@Length
 	String idNumber;
 
-	@ApiModelProperty(required = true)
+	@ApiModelProperty(required = true, example = "张三")
 	@NotEmpty @Size(min = 2)
 	String name;
 }
--- backend/sustech-regency-main/src/main/java/com/sustech/regency/model/param/ReserveParam.java
+++ backend/sustech-regency-main/src/main/java/com/sustech/regency/model/param/ReserveParam.java
@@ -13,16 +13,16 @@ import java.util.List;
 @Data
 public class ReserveParam {
 
-	@ApiModelProperty(value = "房间Id",required = true)
+	@ApiModelProperty(value = "房间Id",required = true, example = "1")
 	@NotNull
 	Integer roomId;
 
-	@ApiModelProperty(value = "预定开始时间",required = true)
+	@ApiModelProperty(value = "预定开始时间",required = true, example = "2022-12-10")
 	@DateParam
 	@NotNull
 	Date startTime;
 
-	@ApiModelProperty(value = "预定结束时间",required = true)
+	@ApiModelProperty(value = "预定结束时间",required = true, example = "2022-12-13")
 	@DateParam
 	@NotNull
 	Date endTime;
