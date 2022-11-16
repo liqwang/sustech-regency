9d6a8a756bdb31f82ef491259e3d9fcb3038f5b8
hh1102 <12012702@mail.sustech.edu.cn>
2022-11-16 17:55:40 +0800

    add floor graph in user page

--- frontend/src/pages/merchantScroll.vue
+++ frontend/src/pages/merchantScroll.vue
@@ -113,20 +113,20 @@
 
           <div v-show="v2" style="width: 100%; height: 100%">
             <!-- <el-image v-show="v1" style="width: 100%; height: 10vh" src='../images/floor2.png' /> -->
-            <img src="../images/floor2.png" usemap="#floor_2" />
+            <img src="../images/floor_3_2.png" usemap="#floor_2" />
             <map name="floor_2">
-              <area shape="rect" coords="185,120,240,260" @click="click_room('201')">
-              <area shape="rect" coords="256,120,311,260" @click="click_room('202')">
-              <area shape="rect" coords="327,120,382,260" @click="click_room('203')">
-              <area shape="rect" coords="398,120,453,260" @click="click_room('204')">
-              <area shape="rect" coords="469,120,524,260" @click="click_room('205')">
-              <area shape="rect" coords="540,120,595,260" @click="click_room('206')">
-              <area shape="rect" coords="611,120,666,260" @click="click_room('207')">
-              <area shape="rect" coords="682,120,737,260" @click="click_room('208')">
-              <area shape="rect" coords="185,315,240,430" @click="click_room('209')">
-              <area shape="rect" coords="256,315,311,430" @click="click_room('210')">
-              <area shape="rect" coords="327,315,382,430" @click="click_room('211')">
-              <area shape="rect" coords="398,315,453,430" @click="click_room('212')">
+              <area shape="rect" coords="35,50,100,180" @click="click_room('201')" >
+              <area shape="rect" coords="100,50,165,180" @click="click_room('202')">
+              <area shape="rect" coords="165,50,230,180" @click="click_room('203')">
+              <area shape="rect" coords="230,50,295,180" @click="click_room('204')">
+              <area shape="rect" coords="375,250,445,390" @click="click_room('205')">
+              <area shape="rect" coords="445,250,515,390" @click="click_room('206')">
+              <area shape="rect" coords="515,250,585,390" @click="click_room('207')">
+              <area shape="rect" coords="585,250,655,390" @click="click_room('208')">
+              <!-- <area shape="rect" coords="185,315,240,430" @click="click_room('209')"> -->
+              <!-- <area shape="rect" coords="256,315,311,430" @click="click_room('210')"> -->
+              <!-- <area shape="rect" coords="327,315,382,430" @click="click_room('211')"> -->
+              <!-- <area shape="rect" coords="398,315,453,430" @click="click_room('212')"> -->
             </map>
           </div>
 
@@ -139,17 +139,11 @@
               <area shape="rect" coords="485,100,555,225" @click="click_room('302')">
               <area shape="rect" coords="555,100,625,225" @click="click_room('303')">
               <area shape="rect" coords="625,100,695,225" @click="click_room('304')">
-
-
+              
               <area shape="rect" coords="60,300,130,435" @click="click_room('305')">
               <area shape="rect" coords="130,300,200,435" @click="click_room('306')">
               <area shape="rect" coords="200,300,270,435" @click="click_room('307')">
               <area shape="rect" coords="270,300,340,435" @click="click_room('308')">
-
-              <!-- <area shape="rect" coords="185,315,240,430" @click="click_room('309')"> -->
-              <!-- <area shape="rect" coords="256,315,311,430" @click="click_room('310')"> -->
-              <!-- <area shape="rect" coords="327,315,382,430" @click="click_room('311')"> -->
-              <!-- <area shape="rect" coords="398,315,453,430" @click="click_room('312')"> -->
             </map>
           </div>
           <!-- <div id="chart" style="width:80%;height:60%"></div> -->
