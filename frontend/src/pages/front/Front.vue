c94f8802ac0861c4d1ff95be15836f51fd94d3b5
Fan <YangFanCool@outlook.com>
2022-11-24 23:34:00 +0800

    beautify front page ui

--- frontend/src/pages/front/Front.vue
+++ frontend/src/pages/front/Front.vue
@@ -1,111 +1,232 @@
 <template>
-  <div class="header">
+  <div id="bgi">
     <el-row>
-      <el-col :span="6" :offset="2" background-color="red"> SUSTECH Rengency</el-col>
-      <el-col :span="3" :offset="3">国内搜索</el-col>
-      <el-col :span="4" :offset="6">
-        <UserIcon />
-      </el-col>
-    </el-row>
-    <el-carousel height="500px" align="center">
-      <el-carousel-item v-for="url in urls" :key="url">
-        <h3 class="small justify-center" text="2xl">
-          <el-image :src="url" />
-        </h3>
-      </el-carousel-item>
-    </el-carousel>
-  </div>
-  <div>
-    <el-row justify="space-evenly">
-      <el-col :span="4">
-        省
-        <el-select v-model="province" placeholder="Select">
-          <el-option v-for="province in provinces" :key="province" :value="province" @click="changeCity(province)" />
-        </el-select>
-      </el-col>
+      <div id="head">
+        <!-- 酒店名字 -->
+        <span style="margin-left: 1vw; font-size: 27px; font-weight: 600; color: #fff">SUSTech Regency</span>
+        <!-- 区域选择 -->
+        <div style="width: 60vw; height: 100%; background-color: " id="area">
+          <el-row justify="space-evenly">
+            <el-col :span="8">
+              <span class="info">省</span>
+              <el-select v-model="province" placeholder="Select">
+                <el-option v-for="province in provinces" :key="province" :value="province" @click="changeCity(province)" />
+              </el-select>
+            </el-col>
 
-      <el-col :span="4">
-        市
-        <el-select v-model="city" placeholder="Select">
-          <el-option v-for="city in cities" :key="city" :value="city" @click="changeRegion(province, city)" />
-        </el-select>
-      </el-col>
+            <el-col :span="8">
+              <span class="info">市</span>
+              <el-select v-model="city" placeholder="Select">
+                <el-option v-for="city in cities" :key="city" :value="city" @click="changeRegion(province, city)" />
+              </el-select>
+            </el-col>
 
-      <el-col :span="4">
-        区
-        <el-select v-model="region" placeholder="Select">
-          <el-option v-for="region in regions" :key="region" :value="region" />
-        </el-select>
-      </el-col>
-
-      <el-col :span="4">
-        <el-button type="primary" style="width: 100px; margin-left: 10px;" :icon="Search" @click="search"
-          :disabled="listLoading">
-          查询
-        </el-button>
-      </el-col>
-    </el-row>
-  </div>
-  <br />
-  <div>
-    <el-row v-for="hotelInfo in hotelInfos" justify="space-evenly" :key="hotelInfo" class="mb-5">
-      <el-col v-for="col in 3" :key="col" :span="7">
-        <div style="boxShadow: var(--el-box-shadow); padding: 10px;">
-          <!-- TODO: -->
-          <router-link :to='"/hotel/" + hotelInfo.id' target="_blank">
-            <el-image :src="url" />
-            南科大专家公寓
-          </router-link>
-          <span class="ml-12">{{ hotelInfo.commentNum }}条评论</span>
-          <span class="ml-12">￥{{ hotelInfo.minPrice }}起</span>
-          <br>
-          <el-rate v-model="hotelInfo.stars" disabled show-score text-color="#ff9900" score-template="{value}分/5分" />
+            <el-col :span="8">
+              <span class="info">区</span>
+              <el-select v-model="region" placeholder="Select">
+                <el-option v-for="region in regions" :key="region" :value="region" />
+              </el-select>
+            </el-col>
+          </el-row>
         </div>
-      </el-col>
+        <!-- 用户头像 -->
+        <div style="height: 100%; align-self: flex-end; display: inline-flex; align-items: center">
+          <UserIcon />
+        </div>
+      </div>
+    </el-row>
+
+    <el-row>
+      <div id="main">
+        <el-row style="height: 100%">
+          <el-col :span="12" :offset="0">
+            <div style="height: 2vh"></div>
+            <div class="null">
+              <div style="height: 9vh"></div>
+              <div style="height: 67vh; width: 100%">
+                <el-carousel height="60vh" align="center" type="" direction="vertical">
+                  <el-carousel-item v-for="url in urls" :key="url">
+                    <el-image :src="url" style="opacity: 1" />
+                  </el-carousel-item>
+                </el-carousel>
+              </div>
+            </div>
+            <div style="height: 3vh"></div
+          ></el-col>
+          <el-col :span="12" :offset="0">
+            <div style="height: 2vh"></div>
+            <div class="null">
+              <div style="height: 72vh; width: 100%">
+                <el-row>
+                  <el-col :span="12" v-for="hotelInfo in hotelInfos">
+                    <el-card class="box-card" shadow="hover" style="border-radius: 10px">
+                      <router-link :to="'/hotel/' + hotelInfo.id" target="_blank">
+                        <div><el-image :src="url" /></div>
+                        <el-row>
+                          <el-col :span="12" :offset="0">
+                            <div class="name" s>{{ hotelInfo.name }}</div>
+                          </el-col>
+                          <el-col :span="12" :offset="0">
+                            <div class="rate">
+                              <el-rate v-model="hotelInfo.stars" disabled text-color="#ff9900" />
+                            </div>
+                          </el-col>
+                          <el-col :span="12" :offset="0" class="comment">
+                            <div class="comment">
+                              <div>共{{ hotelInfo.commentNum }}条评论</div>
+                            </div>
+                          </el-col>
+                          <el-col :span="12" :offset="0" class="price">
+                            ¥<span class="">{{ hotelInfo.minPrice }}</span
+                            >起
+                          </el-col>
+                        </el-row>
+                      </router-link>
+                    </el-card>
+                  </el-col>
+                </el-row>
+
+                <el-pagination
+                  v-model:currentPage="currentPage4"
+                  v-model:page-size="pageSize4"
+                  :page-sizes="[100, 200, 300, 400]"
+                  :small="small"
+                  :disabled="disabled"
+                  :background="background"
+                  layout="total, sizes, prev, pager, next, jumper"
+                  :total="400"
+                  @size-change="handleSizeChange"
+                  @current-change="handleCurrentChange"
+                  style="margin-top: 15px"
+                />
+              </div>
+            </div>
+            <div style="height: 3vh"></div
+          ></el-col>
+        </el-row>
+      </div>
+    </el-row>
+
+    <el-row>
+      <div id="foot">
+        <p style="color: #fff">
+          <span>广东省深圳市南山区学苑大道1088号　</span>
+          <span>电话： +86-755-88010888 &nbsp;</span>
+          <span>邮编： 518055 </span>
+        </p>
+        <p style="color: #fff">© 2022 All Rights Reserved.</p>
+      </div>
     </el-row>
   </div>
-  <div class="ml-12" style="margin-bottom: 200px;">
-    <el-pagination v-model:currentPage="currentPage4" v-model:page-size="pageSize4" :page-sizes="[100, 200, 300, 400]"
-      :small="small" :disabled="disabled" :background="background" layout="total, sizes, prev, pager, next, jumper"
-      :total="400" @size-change="handleSizeChange" @current-change="handleCurrentChange" />
-  </div>
-  <el-divider></el-divider>
-  <el-row style="margin-bottom: 100px;" justify="space-evenly">关于我们 | 联系我们 | 联系客服</el-row>
-  <el-row type="flex" style="margin-bottom: 100px;" justify="space-evenly">
-    <el-col :offset="2" :span="5">广东省深圳市南山区学苑大道1088号 </el-col>
-    <el-col :offset="2" :span="4">电话: +86-755-88010114</el-col>
-    <el-col :offset="2" :span="4">邮编: 518055</el-col>
-  </el-row>
-
 </template>
 
+<style scoped>
+.name {
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-start;
+  font-size: 22px;
+  font-weight: 600;
+}
+.rate {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: flex-end;
+}
+.comment {
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-start;
+  align-items: center;
+  font-size: 14px;
+  color: rgb(66, 66, 66);
+}
+.price {
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-end;
+  text-align: center;
+  align-items: center;
+  font-size: 17px;
+  color: #2a598a;
+}
+
+.null {
+  margin-left: 10px;
+  margin-right: 10px;
+  height: auto;
+}
+.info {
+  margin-left: 1vw;
+  margin-right: 1vw;
+  color: rgb(205, 204, 204);
+}
+#head {
+  width: 100%;
+  height: 8vh;
+  margin-bottom: 1vh;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  background-color: #2a598a;
+}
+#area {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+}
+#bgi {
+  background-image: url('https://withpinbox.com/static/media/bg.aab24a9d.png');
+}
+#main {
+  width: 100%;
+  height: 81vh;
+}
+#foot {
+  width: 100%;
+  height: 9vh;
+  margin-top: 1vh;
+  background-color: #2a598a;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: space-evenly;
+  align-items: center;
+
+  font-weight: 350;
+  font: #fff;
+}
+</style>
+
 <script setup lang="ts">
-import { reactive } from 'vue';
-import UserIcon from '../../components/UserIcon.vue';
+import { reactive } from 'vue'
+import UserIcon from '../../components/UserIcon.vue'
 import request from '../../utils/request'
 import { Search } from '@element-plus/icons-vue'
 
 interface Hotel {
-  address: string,
-  cityName: string,
-  coverUrl: string,
-  id: number,
-  latitude: number,
-  longitude: number,
-  name: string,
-  pictureUrls: string[],
-  provinceName: string,
-  regionName: string,
-  stars: number,
-  tel: string,
+  address: string
+  cityName: string
+  coverUrl: string
+  id: number
+  latitude: number
+  longitude: number
+  name: string
+  pictureUrls: string[]
+  provinceName: string
+  regionName: string
+  stars: number
+  tel: string
   videoUrls: string[]
 }
 
 interface HotelInfo {
-  id: number,
-  name: string,
-  stars: number,
-  commentNum: number,
+  id: number
+  name: string
+  stars: number
+  commentNum: number
   minPrice: number
 }
 
@@ -145,24 +266,20 @@ const urls = [
   'https://z1.muscache.cn/im/pictures/miso/Hosting-45337054/original/d0e063e3-f760-4160-8f56-ec1dcc7d7392.jpeg?aki_policy=large',
   'https://z1.muscache.cn/im/pictures/c7e7c673-9673-4d08-be5a-ce7c2c7143dd.jpg?aki_policy=large'
 ]
+const listLoading = $ref(false)
+const search = () => {}
 
 let provinces = $ref<string[]>([])
 let cities = $ref<string[]>([])
 let regions = $ref<string[]>([])
 
-const listLoading = $ref(false)
-
-const search = () => {
-
-}
-
-request.get('/public/province/all').then(res => {
+request.get('/public/province/all').then((res) => {
   const provinceList = res.data.data as City[]
-  provinces = provinceList.map(p => p.name)
+  provinces = provinceList.map((p) => p.name)
 })
 
 const getHotels = (provinceName: string, cityName: string, regionName: string) => {
-  request.get(`/public/get-hotels-by-location?CityName=${cityName}&&ProvinceName=${provinceName}&&RegionName=${regionName}`).then(res => {
+  request.get(`/public/get-hotels-by-location?CityName=${cityName}&&ProvinceName=${provinceName}&&RegionName=${regionName}`).then((res) => {
     hotels = res.data.data
     console.log('response data: ', hotels)
     hotels.forEach(async (hotel) => {
@@ -178,7 +295,7 @@ const getHotels = (provinceName: string, cityName: string, regionName: string) =
         minPrice: price
       }
       hotelInfos.push(hotelInfo)
-      console.log("hotelInfos", hotelInfos)
+      console.log('hotelInfos', hotelInfos)
     })
     console.log(hotels)
   })
@@ -187,11 +304,11 @@ const getHotels = (provinceName: string, cityName: string, regionName: string) =
 getHotels('', '', '')
 
 const changeCity = (province: string) => {
-  console.log("myprovince: " + province)
+  console.log('myprovince: ' + province)
   getHotels(province, '', '')
-  request.get(`/public/city/all?province=${province}`).then(res => {
+  request.get(`/public/city/all?province=${province}`).then((res) => {
     const cityList = res.data.data as Province[]
-    cities = cityList.map(c => c.name)
+    cities = cityList.map((c) => c.name)
     city = ''
     region = ''
     console.log(cities)
@@ -200,9 +317,9 @@ const changeCity = (province: string) => {
 
 const changeRegion = (province: string, city: string) => {
   getHotels(province, city, '')
-  request.get(`/public/region/all?province=${province}&&city=${city}`).then(res => {
+  request.get(`/public/region/all?province=${province}&&city=${city}`).then((res) => {
     const regionList = res.data.data as Region[]
-    regions = regionList.map(c => c.name)
+    regions = regionList.map((c) => c.name)
     region = ''
     console.log(cities)
   })
@@ -221,10 +338,3 @@ const handleCurrentChange = (val: number) => {
   console.log(`current page: ${val}`)
 }
 </script>
-
-<style scoped>
-.header {
-  margin-top: 10px;
-  margin-bottom: 30px;
-}
-</style>
