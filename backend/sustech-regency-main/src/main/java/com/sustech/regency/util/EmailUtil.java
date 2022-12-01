0d8e56da971aba0b475e157e8f05668c24996de7
WLQ <836200779@qq.com>
2022-12-01 16:24:47 +0800

    Backend: Update `EmailUtil`

--- backend/sustech-regency-main/src/main/java/com/sustech/regency/util/EmailUtil.java
+++ backend/sustech-regency-main/src/main/java/com/sustech/regency/util/EmailUtil.java
@@ -6,6 +6,7 @@ import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.Resource;
+import java.util.List;
 
 @Component
 public class EmailUtil {
@@ -16,9 +17,16 @@ public class EmailUtil {
 	private String from;
 
 	public void sendMail(String to,String content,String subject){
+		sendMail(List.of(to),content,subject);
+	}
+
+	/**
+	 * 缺点：每个收件人都可以看到所有收件人的邮箱，容易暴露隐私
+	 */
+	public void sendMail(List<String> to, String content, String subject){
 		SimpleMailMessage message = new SimpleMailMessage();
 		message.setFrom(from);
-		message.setTo(to);
+		message.setTo(to.toArray(new String[0]));
 		message.setSubject(subject);
 		message.setText(content);
 		mailSender.send(message);
